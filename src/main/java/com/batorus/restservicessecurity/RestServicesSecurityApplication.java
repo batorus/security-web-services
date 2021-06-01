4c9550e2f65153ab6fb5447396e498622c528b09
batorus <batoru@yahoo.com>
2021-06-01 15:39:27 +0300

    first commit

---
+++ src/main/java/com/batorus/restservicessecurity/RestServicesSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.batorus.restservicessecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicesSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServicesSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/batorus/restservicessecurity/RestServicesSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batorus.restservicessecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicesSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
